395e1644a191f4d8a1efa19db644761ef3920d91
generaltune <577174042@qq.com>
2020-03-28 10:36:27 +0800

    配置运行测试脚本以及构建脚本

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-
-set -ex
-wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
-tar -xzvf protobuf-2.4.1.tar.gz
-cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
-
+# 测试用例
+bazel  test  //test:server_test
+# 项目构建
+bazel  build //src/protos:user_cc_grpc
 
