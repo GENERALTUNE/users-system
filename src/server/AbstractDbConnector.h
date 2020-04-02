013217507c27e4b0d2aac76ea0897b58a96a296e
generaltune <577174042@qq.com>
2020-04-02 20:11:21 +0800

    解决依赖路径问题

--- src/server/AbstractDbConnector.h
+++ src/server/AbstractDbConnector.h
@@ -5,7 +5,8 @@
 #include <string>
 #include <memory>
 #include <vector>
-#include "usersystem.pb.h"
+// #include "usersystem.pb.h"
+#include "src/protos/usersystem.grpc.pb.h";
 
 class AbstractDbConnector {
 
